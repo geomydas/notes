09fefafe142f49dced8a625e8bf2aea214e64c58
geomydas <155147963+geomydas@users.noreply.github.com>
2024-10-08 19:08:33 +0800

    updated

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
-module.exports = function (eleventyConfig) {
+module.exports = async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles.css");
+  const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
+  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   return {
     dir: {
       input: "src",
