a66e56c10cab398e7ad1ae1a246f1134acdb179b
geomydas <155147963+geomydas@users.noreply.github.com>
2024-10-09 19:53:57 +0800

    updated shit

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,10 @@
+const CleanCSS = require("clean-css");
+
 module.exports = async function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles.css");
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles;
+  });
   const { EleventyHtmlBasePlugin } = await import("@11ty/eleventy");
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   return {
